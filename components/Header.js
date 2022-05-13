f0b47f02a1262b7ba76e40ce104f53fc7f126130
Live <41640774+LiveZX@users.noreply.github.com>
2022-05-13 19:16:27 +0200

    Added header elements, buttons + bouncy

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,29 @@
 /* eslint-disable jsx-a11y/alt-text */
+import {
+
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon,
+} from '@heroicons/react/outline';
+
 import React from 'react'
 import Image from 'next/image'
+import HeaderItem from './HeaderItem'
 
 function Header() {
   return (
     <header className=''> 
-
-    <h1>This is the header.</h1>
+    <div>
+        <HeaderItem title='HOME' Icon={HomeIcon} />
+        <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+        <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+        <HeaderItem title='COLLECTION' Icon={CollectionIcon} />
+        <HeaderItem title='SEARCH' Icon={SearchIcon} />
+        <HeaderItem title='ACCOUNT' Icon={UserIcon} />
+    </div>
     <Image 
     className="object-contain"
     src='https://links.papareact.com/ua6'
